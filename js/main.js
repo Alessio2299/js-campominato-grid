63671bbd4e0164993941f7fc9171ff0220e962a4
Alessio Napoli <alessionapoli99@icloud.com>
2022-01-31 16:37:37 +0100

    Added Function "AddEventListener", Conditions, and added cycle For

--- js/main.js
+++ js/main.js
@@ -10,7 +10,40 @@ let levels = document.getElementById("levels");
 let difficulty = "";
 
 
+// Creare un evento sul bottone play per avviare il gioco
+button.addEventListener("click",
+  function(){
 
+    // Stabilire delle condizioni in base alla difficoltà scelta
+    if(levels.value == "easy"){
+      difficulty = 100;
+      box = "box-small"; 
+    } else if(levels.value == "hard"){
+      difficulty = 81;
+      box = "box-medium"; 
+    } else{
+      difficulty = 49;
+      box = "box-big";  
+    }
+    console.log(box);
+    // Creare un ciclo for, per stampare un tot di box in base alla difficoltà selezionata
+    for(let i = 0; i<difficulty; i++){
+      
+      const div = document.createElement("div");
+
+      // Inserire le classi che mi servono
+      container.appendChild(div);
+      div.classList.add(box);
+      div.classList.add("box");
+
+      div.addEventListener("click",
+        function(){
+          div.classList.add("active")
+        }
+      )
+    }
+  }
+)
 
 
 

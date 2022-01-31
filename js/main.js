002c58a1556ce221d276d05d04b2b4aa54b5f0f5
Alessio Napoli <alessionapoli99@icloud.com>
2022-01-31 16:34:42 +0100

    Created main variables

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// Creare le variabili che inzialmente mi posso servire
+
+let container = document.getElementById("container");
+const button = document.getElementById("play");
+let box = "";
+
+
+// Salvare il valore della difficolta scelta dal giocatore
+let levels = document.getElementById("levels");
+let difficulty = "";
+
+
 
 
 
